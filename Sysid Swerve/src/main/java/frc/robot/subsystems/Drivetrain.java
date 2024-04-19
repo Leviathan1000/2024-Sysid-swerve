a8a5e77f05101b8fbc6f4b76057779044fbe78be
ChairmanMeow <dmweg10@hotmail.com>
2024-04-18 20:26:05 -0500

    better conversion factor

--- Sysid Swerve/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Sysid Swerve/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -17,18 +18,18 @@ public class Drivetrain extends SubsystemBase {
   private CANSparkMax BRMotor = new CANSparkMax(1, MotorType.kBrushless);
   private CANSparkMax BLMotor = new CANSparkMax(3, MotorType.kBrushless);
 
-
-
+  private double gearRatio = 6.12/1;
+  private double metersPerWheelRotation = 2 * Math.PI * Units.inchesToMeters(4);
   /** Creates a new Drivetrain. */
   public Drivetrain() {
     BRMotor.follow(FRMotor);
     BLMotor.follow(FLMotor);
     
-    FRMotor.getEncoder().setPositionConversionFactor(0.05215454470665408);
-    FLMotor.getEncoder().setPositionConversionFactor(0.05215454470665408);
+    FRMotor.getEncoder().setPositionConversionFactor(gearRatio/metersPerWheelRotation); // rotations per meter
+    FLMotor.getEncoder().setPositionConversionFactor(gearRatio/metersPerWheelRotation);
 
-    FRMotor.getEncoder().setVelocityConversionFactor(0.05215454470665408 * 60);
-    FLMotor.getEncoder().setVelocityConversionFactor(0.05215454470665408* 60);
+    FRMotor.getEncoder().setVelocityConversionFactor(gearRatio/metersPerWheelRotation * 60);
+    FLMotor.getEncoder().setVelocityConversionFactor(gearRatio/metersPerWheelRotation * 60);
   }
 
   @Override
